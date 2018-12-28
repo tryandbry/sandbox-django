c61b77243432c5aae0db40db676b76189b130c12
Bryan <bryan@localhost.localdomain>
2018-12-27 19:53:47 -0600

    adding custom method for Question

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
@@ -11,6 +13,9 @@ class Question(models.Model):
 
   def __str__(self):
     return self.question_text
+
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
 @python_2_unicode_compatible
 class Choice(models.Model):
